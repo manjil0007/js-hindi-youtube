64d18ec67e9bc43a72dba491ca38c67b08770398
manjil <manjilkc216@gmail.com>
2024-11-18 08:43:22 +0530

    control config done

--- .devcontainer/02_basics/04_objects.js
+++ .devcontainer/02_basics/04_objects.js
@@ -1,6 +1,52 @@
+const { compareSync } = require("bcryptjs");
+
 const instaUser = new Object()
 
 instaUser.id="123abc"
-tinderUser.name = "Sam"
+instaUser.name = "Sam"
+instaUser.isLoggedIn = false
 
+// console.log(instaUser);
+
+const regularUser = {
+    email:"Some@gmmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Manjil",
+            lastname:"KC"
+        }
+    }
+}
+
+//console.log(regularUser.fullname)
+
+
+const obj1 = { 1:"a", 2:"b"}
+const obj2 = { 3:"a", 4:"b"}
+const obj4 = { 3:"a", 4:"b"}
+
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+// console.log(obj3)
+const obj3 = {...obj1,...obj2}
+//  console.log(obj3)
+
+const users = [
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:3,
+        email:"m@gmail.com"
+    }
+]
+console.log(users)
 console.log(instaUser);
+console.log(Object.keys(instaUser));
+console.log(Object.values(instaUser));
+console.log(Object.entries(instaUser));
+console.log(instaUser.hasOwnProperty("isLoggedIn"));
+
+
+
+
